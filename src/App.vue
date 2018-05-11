ccd67a7acab410b19318510c4a985574d6e8f9cd
user <vmarkunin@ncem.ru>
2018-05-11 17:47:21 +0300

    initial templates

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,46 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer
+            v-model="drawer"
+            fixed
+            app
+            mobile-break-point="1940"
+    >
+      <v-list dense>
+        <v-list-tile :to="{name: 'home'}">
+          <v-list-tile-action>
+            <v-icon>home</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Главная</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar color="blue darken-4" dark fixed app height="90">
+      <v-toolbar-side-icon @click.stop="drawer = !drawer" class="display-3"></v-toolbar-side-icon>
+      <v-toolbar-title>
+        <v-flex d-flex align-center justify-start>
+          <span class="body-2 ml-2 pr-4" style="border-right: 1px #fff solid;">МИНИСТЕРСТВО<br>ЗДРАВООХРАНЕНИЯ РФ</span>
+          <span class="body-1 ml-4">ИНФОРМАЦИОННО-АНАЛИТИЧЕСКАЯ СИСТЕМА МОНИТОРИНГА<br>И КОНТРОЛЯ В СФЕРЕ ЗАКУПОК ЛЕКАРСТВЕННЫХ ПРЕПАРАТОВ</span>
+        </v-flex>
+      </v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid fill-height>
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+    export default {
+        data: () => ({
+            drawer: null
+        }),
+        props: {
+            source: String
+        }
     }
-  }
-}
-</style>
+</script>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,23 @@
 import Vue from 'vue'
+import Vuetify from 'vuetify'
+import VeeValidate from 'vee-validate';
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
 
+import 'vuetify/dist/vuetify.min.css'
+import 'babel-polyfill'
+import './styles/index.scss'
+
+Vue.use(Vuetify)
+Vue.use(VeeValidate);
+
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
